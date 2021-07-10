68602f261d6b1c745aae93fe81b0b0d01ecfb29c
Carl Åberg <mail.carl.aberg@gmail.com>
2021-07-10 10:47:22 +0200

    Added netlify functions uri to apollo client

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -4,6 +4,6 @@ import {
 } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: ' https://rzoa23w8f0.execute-api.us-east-1.amazonaws.com/dev/graphql',
+    uri: ' /.netlify/functions/graphql',
     cache: new InMemoryCache()
 });
