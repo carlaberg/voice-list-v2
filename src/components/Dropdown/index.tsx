a022a73dca387d6cc06eca0d919a90b52c883010
Carl Åberg <mail.carl.aberg@gmail.com>
2021-08-25 09:09:05 +0200

    Started on dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../Card'
+
+const Dropdown = (props) => {
+  return (
+    <Card>
+      {props.children}
+    </Card>
+  )
+}
+
+export default Dropdown
